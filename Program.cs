f8d41152b21448d5d068ab16327620d22006b1ad
PaoloPci <paolopci@yahoo.it>
2024-03-01 14:47:44 +0100

    002 Create Gateway Project and 003 Configure Application to use Ocelot

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// aggiungo Ocelot
+builder.Services.AddOcelot();
+
+
 app.MapGet("/", () => "Hello World!");
 
+// Ocelot
+app.UseOcelot();
 app.Run();
