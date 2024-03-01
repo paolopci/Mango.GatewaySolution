b86db768b81724073dac1f949a85a9c8cf53a35f
PaoloPci <paolopci@yahoo.it>
2024-03-01 14:56:52 +0100

    004 Add Authentication to Ocelot

--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
+using Mango.GatewaySolution.Extensions;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
+// USO L'autenticazione che ho dentro la cartella extension
+builder.AddAppAuthentication();
 // aggiungo Ocelot
 builder.Services.AddOcelot();
+var app = builder.Build();
+
 
 
 app.MapGet("/", () => "Hello World!");
